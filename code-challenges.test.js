862c48e3cf68cb2642a47db5e6c097c83ac42672
erik-engstrom <eekareeka@yahoo.com>
2021-05-24 08:24:53 -0700

    week 3 assessment completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding practical questions with Jest
 
+// const { LOGICAL_OPERATORS } = require("@babel/types")
+
 // Please read all questions thoroughly
 // Pseudo coding is HIGHLY recommended
 // If you get stuck, please leave comments to help us understand your thought process
@@ -18,19 +20,32 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-// Example input: 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-// Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-
-
-
+// The testing method starts by adding the function name to the describe line  
+describe ("fibonacci", () => {
+  //declare the variables to be used
+  let input1 = 6
+  let input2 = 10
+  // write a test for what you are looking to return in plain words
+  test ("return an array of numbers that add the previous 2 indexes to make the 3rd index", () => {
+  // next is what you expect the function to return when evaluated
+    expect(fibonacci(input1)).toEqual("[1, 1, 2, 3, 5, 8]")
+    expect(fibonacci(input2)).toEqual("[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]")
+  })
+})
 
 // b) Create the function that makes the test pass.
-
-
+// set initial conditions
+let n1 = 0, n2 = 1, nextTerm;
+// Define function to return fibonacci sequence
+const fibonacci= (num) => {
+  //write a for loop to go over the length of the input and add the terms 
+  for (let i = 1; i <= num; i++){
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+    return n1;
+  }  
+};
 
 
 
@@ -38,36 +53,75 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// The testing method starts by adding the function name to the describe line
+describe ("oddSorted", () => {
+
+  //declare the variables to be used
+  var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
 
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+  var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+// write a test for what you are looking to return in plain words
+test ("return a new array of only odd numbers sorted from least to greatest", () => {
+  // next write what you expect the function to return when evaluated
+    expect(oddsSorted(fullArr1)).toEqual("[-9, 7, 9, 199]")
+    // Excpected output: [-9, 7, 9, 199]
+    expect(oddsSorted(fullArr2)).toEqual("[-823, 7, 23]")
+    //  // Expected output: [-823, 7, 23]
 
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-
-
-
+// write a function that returns only odd numbers in order from least to greatest.
+const oddsSorted = (arr) =>{
+  // use the .filter method to evaluate over the array to return a new array not the same length.
+  arr.filter(arr => 
+  //write a condition where only odd numbers are returned and sorted in ascending order
+    if (arr % 3 === 0 && arr !== "" && arr !== true && arr !== false && arr !== null){
+      arr.sort(function(a, b){return a - b});
+    }
+  }
+}
+// Can't get the test to pass but I think I'm on the right path...
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
-var numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-var numbersToAdd3 = []
-// Expected output: []
-
-
+// The testing method starts by adding the function name to the describe line  
+describe ("addArray", () => {
+  //declare the variables to be used
+  var numbersToAdd1 = [2, 4, 45, 9]
+  var numbersToAdd2 = [0, 7, -8, 12]
+  var numbersToAdd3 = []
+  
+  // write a test for what you are looking to return in plain words
+  test ("return an array of numbers that accumulate the sum, an empty array should return an empty array", () => {
+  // next write what you expect the function to return when evaluated
+    expect(addArray(numbersToAdd1)).toEqual("[2, 6, 51, 60]")
+    // Excpected output: [2, 6, 51, 60]
+    expect(addArray(numbersToAdd2)).toEqual("[0, 7, -1, 11]")
+    //  // Expected output: [0, 7, -1, 11]
+    expect(addArray(numbersToAdd3)).toEqual("[]")
+    // Expected output: []
+  })
+})
 
 
 
 // b) Create the function that makes the test pass.
+
+// Define a function that takes in one argument
+const addArray = (array) =>{
+// was thinking about using .map() method here but I found the .reduce() to maybe work better in this case.
+  array.reduce(function(r, a) {
+  if (r.length > 0)
+    a += r[r.length - 1];
+  r.push(a);
+  return r;
+}, []);
+}
+// I may have messed up the logic of the reduce method. Can't get the test to pass but once again feel like i'm pretty close. 
--- interview-questions.md
+++ interview-questions.md
@@ -9,41 +9,41 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are React lifecycle methods? Provide three examples.
 
-  Your answer:
+  Your answer:The react lifestyle methods are the life cycle of a component from initialization to changing state and then death where the component is no longer used. 3 examples would be the constructor, changing state and setting state.
 
-  Researched answer:
+  Researched answer: Each component in React has its own lifecycle that can be monitored and manipulated. It's 3 main phases are mounting, updating and unmounting. In each of those phases there are multiple built-in methods such as: constructor(), render() getDerivedStateFromProps(), componentDidMount(), shouldComponentUpdate(), getSnapshotBeforeUpdate() and componentWillUnmount() for the unmounting phase.  
 
 
 
 2. What is the difference between a HTML/JSX div and a span?
 
-  Your answer:
+  Your answer: an HTML div and span are used as the structure for your document and JSX adds javascript funtionality to the div and span tags. 
 
-  Researched answer:
+  Researched answer: The main differenece between a div and span tag is that a div tag is for wrapping block elements and a span is used as an inline element. 
 
 
 
 3. What is a ternary operator in JavaScript?
 
-  Your answer:
+  Your answer: A ternary operator is a short hand if statement that can be written in one line. For example to the question is a number even or odd can be written as a ternary operator by let isEven = num %2 ===0 ? "yes num is even":"no num is odd"
 
-  Researched answer:
+  Researched answer: A ternary operator is a way to shorten a simple if else block and is written as: variable_name = (condition) ? value_if_true : value_if_false
 
 
 
 4. What are props in React?
 
-  Your answer:
+  Your answer: Props in react are inherited properties from the parent class to the child class.
 
-  Researched answer:
+  Researched answer: Props is a keyword in React which stands for properties and is being used for passing data from one component to another. The direction flow is only one-way down from parent to child components. 
 
 
 
 5. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: the "this" in JavaScript is an operator using dot notation "this.state" to access properties in a component to pass to another component.
 
-  Researched answer:
+  Researched answer: In a method the "this" keyword is used to refer to the object that it belongs to. 
 
 
 
@@ -51,26 +51,26 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: An anonymous function is a function that was declared without any name identifier to refer to it. 
 
 
 
 7. STRETCH: What does it mean to create a responsive UI? What are three options for creating responsive design?
 
-  Your answer:
+  Your answer: A responsive UI is one that changes dynamically with the changing of screen widths and heights. All components of the page shold still be in the same location and maintain functionality as the screen shrinks to a mobile or tablet size.
 
-  Researched answer:
+  Researched answer: Responsive design is a design approach used to create content that adjusts smoothly to various screen sizes. The 3 options for creating responsive design are: fluid grid system, fluid image use and media queries.
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Ruby:
+1. Ruby: Ruby is a programming language that balances functional programming with imperative programming. Ruby is an object oriented programming language that offers flexibility and functionality to programming.
 
-2. Object oriented programming:
+2. Object oriented programming: Object oriented programming relies on classes and objects to structure software programming into simple, reusable pieces of code blueprints which are used to create individual instances of objects.
 
-3. RSpec:
+3. RSpec:Rspec is a testing tool for Ruby and is the most frequently used testing library for Ruby.
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are anonymous functions that can be passed into methods.
 
-5. Ruby hashes:
+5. Ruby hashes: A Ruby hash is a collection of key-value pairs.
